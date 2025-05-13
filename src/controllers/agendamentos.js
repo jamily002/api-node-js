f9dac2442d926bd0a423c3d6275bc180f8b30d96
JAMILY VICTORIA SOUSA DOS SANTOS <jamily.santos12>
2025-05-13 11:44:02 -0300

    05-13

--- src/controllers/agendamentos.js
+++ src/controllers/agendamentos.js
@@ -2,10 +2,19 @@ const db = require('../database/connection');
 module.exports = {
     async listarAgendamento(request, response) {
         try {
+
+           const sql = `
+           SELECT agd_id, psi_id, usu_id, agd_data_consulta, agd_inicio_consulta, 
+           agd_fim_consulta, agd_anotacoes_consulta 
+           FROM agendamentos;
+           `;
+           const [ rows] = await db.query(sql);
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de Agendamento', 
-                dados: null
+                itens: rows.lenght,
+                dados: rows
             });
         } catch (error) {
             return response.status(500).json({
